4584f064d0a8851791cd437e3dd9e5a5b3a39056
lukejiang <lukejiang@tencent.com>
2021-03-18 11:09:43 +0800

    Sound Monitor

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,56 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-header><img src="./assets/sjtu.jpeg"><span>Intelligentized Robotic Welding Technology Laboratory</span></el-header>
+    <el-main>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="Weld Pool Monitor" name="first">
+          <WeldPool/>
+        </el-tab-pane>
+        <el-tab-pane label="Sound Monitor" name="second">
+          <Sound />
+        </el-tab-pane>
+        <el-tab-pane label="Current and Voltage Monitor" name="third">角色管理</el-tab-pane>
+      </el-tabs>
+    </el-main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeldPool from './components/WeldPool.vue'
+import Sound from './components/Sound.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    WeldPool,
+    Sound,
+  },
+  data() {
+    return {
+      activeName: 'first'
+    };
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: black;
+}
+.el-header{
+  text-align: left;
+  font-size: 20px;
+  font-weight: bolder;
+  margin: 0;
+}
+.el-header img {
+  height: 20px;
+}
+.el-header span {
+  margin: 0 10px;
 }
 </style>
---
+++ src/components/Sound.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="sound">
+    <h1>SOUND SIGNAL MONITOR</h1>
+    <img src="../assets/Sound/sound_sensor.jpg">
+    <el-row>
+      <el-col :span="12">
+        <div>
+          <h2>S-F1</h2>
+          <img src="../assets/Sound/S-F1.png">
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div>
+          <h2>S-E</h2>
+          <img src="../assets/Sound/S-E.png">
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div>
+          <h2>S-F2</h2>
+          <img src="../assets/Sound/S-F2.png">
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div>
+          <h2>S-A</h2>
+          <img src="../assets/Sound/S-A.png">
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div>
+          <h2>S-F3</h2>
+          <img src="../assets/Sound/S-F3.png">
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div>
+          <h2>S-Std</h2>
+          <img src="../assets/Sound/S-Std.png">
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div>
+          <h2>S-F4</h2>
+          <img src="../assets/Sound/S-F4.png">
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div>
+          
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sound',
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1.5em;
+  margin: 10px 0;
+}
+h2 {
+  margin: 0;
+}
+img {
+  width: 20%;
+}
+.el-row {
+  margin: 10px 0;
+}
+.el-row img {
+  width: 90%;
+}
+</style>
